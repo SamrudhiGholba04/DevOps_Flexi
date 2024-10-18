b1216f4b73807c9cf59e5ed5839620f5e71153fa
Samrudhi Gholba <146966148+SamrudhiGholba04@users.noreply.github.com>
2024-10-18 21:43:16 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, World!"  # Change this to your actual application logic
+from application import app
 
 if __name__ == "__main__":
-    app.run(debug=True)  # This runs the app in debug mode
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
